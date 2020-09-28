9dfe7cc6d9e18ee4ecfcfee0cb95bd23e6717fbd
TatianaRo <tatianavln2012@gmail.com>
2020-09-28 16:47:37 -0300

    criação do modelo de filial

---
+++ db/migrate/20200928194658_create_subsidiaries.rb
@@ -0,0 +1,10 @@
+class CreateSubsidiaries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :subsidiaries do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
