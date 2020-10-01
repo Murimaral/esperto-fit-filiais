d1c2e625710636d1cef62360a10f02070413dc26
Felipe Rosa <flp.far@gmail.com>
2020-10-01 16:27:03 -0300

    Linha de expectativa desnecessaria removida, teste de validacao do token da filial
    Co-authored-by: Gabriel <gscmonteiro@gmail.com>

--- spec/models/subsidiary_spec.rb
+++ spec/models/subsidiary_spec.rb
@@ -25,7 +25,6 @@ RSpec.describe Subsidiary, type: :model do
     it 'generates a token on creation' do
       subsidiary = create(:subsidiary)
 
-      expect(subsidiary.token).to be_present
       expect(subsidiary.token.size).to eq(6)
       expect(subsidiary.token).to match(/^[A-Z0-9]+$/)
     end
