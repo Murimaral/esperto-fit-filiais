50623c4bbb9a23817533f7b057c1dca9b8aef0a3
gmonteiro13 <gscmonteiro@gmail.com>
2020-10-02 16:50:29 -0300

    Adicionados testes de autenticacao para ver filial, voltar para index e home page
    Co-authored-by: Felipe <flp.far@gmail.com>

--- spec/features/admin_view_subsidiaries_spec.rb
+++ spec/features/admin_view_subsidiaries_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 feature 'Admin view subsidiaries' do
-  scenario 'successfully' do
+  scenario 'succesfully' do
     user = create(:user)
-    create(:subsidiary, name: 'Osasco')
-    create(:subsidiary, name: 'Pinheiros')
-    create(:subsidiary, name: 'Vila Olímpia')
+    subsidiary = create(:subsidiary, name: 'Osasco')
+    subsidiary2 = create(:subsidiary, name: 'Pinheiros')
+    subsidiary3 = create(:subsidiary, name: 'Vila Olímpia')
 
     login_as user
     visit root_path
@@ -14,6 +14,24 @@ feature 'Admin view subsidiaries' do
     expect(page).to have_content('Osasco')
     expect(page).to have_content('Pinheiros')
     expect(page).to have_content('Vila Olímpia')
+    expect(page).to have_content(subsidiary.token)
+    expect(page).to have_content(subsidiary2.token)
+    expect(page).to have_content(subsidiary3.token)
+  end
+
+  scenario 'and view details' do
+    user = create(:user)
+    subsidiary = create(:subsidiary, name: 'Osasco')
+
+    login_as user
+    visit root_path
+    click_on 'Gerenciar filiais'
+    click_on 'Osasco'
+
+    expect(page).to have_content(subsidiary.name)
+    expect(page).to have_content(subsidiary.address)
+    expect(page).to have_content(subsidiary.cnpj)
+    expect(page).to have_content(subsidiary.token)
   end
 
   scenario 'and there are no subsidiaries' do
@@ -24,5 +42,36 @@ feature 'Admin view subsidiaries' do
     click_on 'Gerenciar filiais'
 
     expect(page).to have_content('Não há filiais cadastradas')
+  end
+
+  scenario 'must be logged in' do
+    visit subsidiaries_path
+
+    expect(page).to have_content('Para continuar, efetue login ou registre-se.')
+  end
+
+  scenario 'and return to index from show' do
+    user = create(:user)
+    subsidiary = create(:subsidiary)
+
+    login_as user
+    visit root_path
+    click_on 'Gerenciar filiais'
+    click_on subsidiary.name
+    click_on 'Voltar'
+
+    expect(current_path).to eq(subsidiaries_path)
+  end
+
+  scenario 'and return to home page from index' do
+    user = create(:user)
+    create(:subsidiary)
+
+    login_as user
+    visit root_path
+    click_on 'Gerenciar filiais'
+    click_on 'Voltar'
+
+    expect(current_path).to eq(root_path)
   end
 end
