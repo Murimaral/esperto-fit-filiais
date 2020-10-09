0a4f6444f4ba17188738faf0af787252b67ccd4b
Felipe Rosa <flp.far@gmail.com>
2020-10-09 12:09:59 -0300

    Adiciona shoulda-matchers em testes unitários

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,30 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  context 'validation' do
-    it 'name must be unique' do
-      create(:product, name: 'Lorem')
-      new_product = build(:product, name: 'Lorem')
-
-      new_product.valid?
-
-      expect(new_product.errors[:name]).to include('já está em uso')
-    end
-
-    it 'price must be a decimal greater than 0' do
-      product = build(:product, min_price: -20)
-
-      product.valid?
-
-      expect(product.errors[:min_price]).to include('deve ser maior que 0')
-    end
-
-    it 'price must be a number' do
-      product = build(:product, min_price: 'abc')
-
-      product.valid?
-
-      expect(product.errors[:min_price]).to include('não é um número')
-    end
+  describe 'validations' do
+    subject { create(:product) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_numericality_of(:min_price).is_greater_than(0) }
   end
 end
