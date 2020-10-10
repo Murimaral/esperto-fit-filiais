a7a82aa24796e8541c0296934c168721ded26e98
TatianaRo <tatianavln2012@gmail.com>
2020-10-10 17:57:35 -0300

    Testes unitários de criação de usuario e profile
    Co-authored-by: Gabriel <gscmonteiro@gmail.com>

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
+  describe 'validations' do
+    subject { create(:profile) }
+    it { should allow_value('924.737.808-70').for(:cpf) }
+    it { should allow_value('92473780870').for(:cpf) }
+    it { should_not allow_value('123.123.000-00').for(:cpf) }
+    it { should validate_presence_of(:full_name) }
+    it { should validate_presence_of(:cpf) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:subsidiary) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should allow_value('email@espertofit.com.br').for(:email) }
+    it { should_not allow_value('email@address.foo').for(:email) }
+    it { should_not allow_value('foo').for(:email) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should_not allow_value('123').for(:password) }
+    it { should validate_presence_of(:role) }
+  end
 end
