a364cfb3bd8ca631a562c6f7a7cd367ca717208e
Felipe Rosa <flp.far@gmail.com>
2020-10-12 09:57:57 -0300

    Adiciona format_cpf before create e rescue_from

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -1,15 +1,22 @@
 class Api::V1::EnrollmentsController < Api::V1::ApiController
+  rescue_from ActionController::ParameterMissing, with: :render_precondition_failed
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
   def create
     @enrollment = Enrollment.new(enrollment_params)
     @enrollment.save!
-    render json: @enrollment, status: :created
-  rescue ActionController::ParameterMissing
-    render status: :precondition_failed, json: 'Parâmetros inválidos'
-  rescue ActiveRecord::RecordInvalid
-    render status: :unprocessable_entity, json: @enrollment.errors.full_messages
+    render status: :created, json: @enrollment
   end
 
   private
+
+  def render_precondition_failed
+    render status: :precondition_failed, json: 'Parâmetros inválidos'
+  end
+
+  def render_unprocessable_entity
+    render status: :unprocessable_entity, json: @enrollment.errors.full_messages
+  end
 
   def enrollment_params
     params.require(:enrollment).permit(:customer_name, :customer_cpf, :email, :subsidiary_plan_id, :price)
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -5,6 +5,7 @@ class Enrollment < ApplicationRecord
 
   before_create :generate_token
   before_create :calculate_valid_thru
+  before_create :format_customer_cpf
 
   validates :price, numericality: { greater_than: 0 }
   validates :customer_cpf, :price, :customer_name, :email, presence: true
@@ -19,9 +20,13 @@ class Enrollment < ApplicationRecord
   end
 
   def customer_cpf_must_be_valid
-    return if customer_cpf.blank? || (CPF.valid?(customer_cpf) && (self.customer_cpf = CPF.new(customer_cpf).formatted))
+    return if customer_cpf.blank? || CPF.valid?(customer_cpf)
 
     errors.add :customer_cpf, :invalid
+  end
+
+  def format_customer_cpf
+    self.customer_cpf = CPF.new(customer_cpf).formatted
   end
 
   def calculate_valid_thru
