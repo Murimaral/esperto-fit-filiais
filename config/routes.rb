705832edf5946184bd718a03acaead901bcd7ac6
gmonteiro13 <gscmonteiro@gmail.com>
2020-10-16 17:39:48 -0300

    Resolvendo problemas de merge (routes com rotas duplicadas) e falhas no RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,10 @@ Rails.application.routes.draw do
 
   resources :enrollments, only: %i[index show] do
     resources :banned_customers, only: %i[new create]
+    get 'search', on: :collection
   end
   resources :plans, only: %i[index new create show edit update]
   resources :profiles, only: %i[index show new create]
-  resources :enrollments, only: %i[index show] do
-    get 'search', on: :collection
-  end
   resources :subsidiaries, only: %i[index show new create] do
     resources :subsidiary_plans, only: %i[new create]
     resources :enrollments, only: %i[index show]
