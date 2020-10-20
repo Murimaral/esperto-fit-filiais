559a1fed94104249d3ea53ab85ce1273416e0dcf
Caio César Valério <ccmvcaio@gmail.com>
2020-10-20 10:20:29 -0300

    tirando return de must_be_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def must_be_admin
-    return redirect_to subsidiary_path(current_user.subsidiary) unless current_user.admin?
+    redirect_to subsidiary_path(current_user.subsidiary) unless current_user.admin?
   end
 end
